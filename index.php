34a39cd719cb10233867f796afca46918e45a7cd
Dustin Quam <quamsta@gmail.com>
2014-01-31 17:03:02 -0600

    grid fixes

--- index.php
+++ index.php
@@ -15,34 +15,37 @@
             </div>
          <div class="row section-container">
 
-                <section class="large-9 large-centered columns">
-
+                <section>
                   <h1>About</h1>
+                  
+                      <div class="large-11 columns">
                          
-                  <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+                      <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
 
-                  <h2>Header Level 2</h2>
-                           
-                  <ol>
-                     <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-                     <li>Aliquam tincidunt mauris eu risus.</li>
-                  </ol>
+                      <h2>Header Level 2</h2>
+                               
+                      <ol>
+                         <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+                         <li>Aliquam tincidunt mauris eu risus.</li>
+                      </ol>
 
-                  <blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p></blockquote>
+                      <blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p></blockquote>
 
-                  <h3>Header Level 3</h3>
+                      <h3>Header Level 3</h3>
 
-                  <ul>
-                     <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-                     <li>Aliquam tincidunt mauris eu risus.</li>
-                  </ul>
+                      <ul>
+                         <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+                         <li>Aliquam tincidunt mauris eu risus.</li>
+                      </ul>
+                    </div>
 
                 </section>
             </div>
-            <div class="row section-container">
-                <section id="news" class="large-10 large-centered columns" data-magellan-destination="news">
+            <div class="row section-container" data-magellan-destination="news">
+
+                <section id="news">
                   <h1>News</h1>
-                         
+                  <div class="large-11">      
                   <ul id="news-list" class="small-block-grid-1 medium-block-grid-2 large-block-grid-3">
                     <li>
                         <article class="has-background" style="background-image: url('images/yacht.jpg');"> 
@@ -83,7 +86,7 @@
                       </article>
                     </li>              
                   </ul>
-
+                </div>
                 </section>
               <!--  <section id="news" class="large-12 large-centered columns" data-magellan-destination="news">
                   <h1>News</h1>
