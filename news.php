1db36c245e9cb423cf2d1443cdf66f4cd24be1d8
Dustin Quam <quamsta@gmail.com>
2014-02-04 09:29:44 -0600

    Merge branch 'blur-effect'

--- news.php
+++ news.php
@@ -1,6 +1,15 @@
 <?php include('includes/top.php'); ?>
 <?php include('includes/header.php'); ?>
-<?php $randomImage = rand(1,6); ?>
+<?php 
+
+	if(isset($_GET['image']) && ($image = $_GET['image'])){
+		$image = 'images/'.$image;
+	}else{
+		$randomImage = rand(1,6);
+		$image = 'images/news-example'.$randomImage.'.jpg';
+	}
+
+?>
 
 <a name="news" data-magellan-destination="news"></a>
 <div class="main single-page" role="main">
@@ -11,20 +20,23 @@
 
 		
 			<div class="row">
-				<div class="article-header" style="background-image: url('images/news-example3.jpg');">
+				<!-- <div class="article-header" style="background-image: url('images/news-example<?php echo $randomImage; ?>.jpg');"> -->
+				<div class="article-header" style="background-image: url('<?php echo $image; ?>');">
 					<!--<img src="images/news-example<?php echo $randomImage; ?>.jpg" />-->
 					<!--<img src="http://dummyimage.com/200x700/f2f2f2/333333" />-->
 					
 				</div>
 			</div>
-
-			<div class="row">
+			<!-- <div class="effect" style="background-image: url('images/news-example<?php echo $randomImage; ?>.jpg');"></div> -->
+			<div class="effect" style="background-image: url('<?php echo $image; ?>');"></div>
+			<div class="row article-container">
+			
+				
 				<!-- If the article has an image, add the "has-image" class below:-->
 				<!-- (alternatively, if you remove the has-image class, you get a more traditional image above text) -->
 				<!--<div class="large-7 article-text has-image columns"> -->
 				<div class="large-7 article-text has-image columns">
-					<div class="effect" style="background-image: url('images/news-example3.jpg');"></div>
-					<ul class="breadcrumbs">
+	<ul class="breadcrumbs">
 				 		<li><a href="index.php#news">News</a></li>
 				  		<li class="current"><a href="#">Going Sailing This Weekend? Let Us Know!</a></li>
 					</ul>
