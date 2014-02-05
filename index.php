2baaa8453d330cd1becbeddcb4a6f72c069fc8a1
Dustin Quam <quamsta@gmail.com>
2014-02-05 16:41:17 -0600

    continuing new header work

--- index.php
+++ index.php
@@ -3,20 +3,17 @@
 
 
       <div class="main" role="main">
-                <div class="effect home" style="background-image: url('images/group2.jpg');"></div>
+                <div class="effect home"></div>
                 <div class="about-hero" data-magellan-destination="about" style=" background-image: url('images/group2.jpg');">
                   <div class="tagline-container">
                     <div class="row">
-                      <div class="large-10 large-centered columns tagline">
-                        <div class="row">
-                          <div class="large-10 columns">
+                      <div class="large-9 large-centered columns tagline">
+
                             <p>The Knitting and Sailing Club throws convention to the wind. We are a small, welcoming student organization focused around the outdoor wet knitting sensation currently sweeping the nation.
-                          </div>
-                          <div class="large-2 columns tagline-links">
-                              <a href="#" class="tagline-link">Join</a>
-                              <a href="#" class="tagline-link">Visit us on OrgSync</a>
-                          </div>
-                        </div>
+                              <div class="tagline-links">
+                                <a href="#" class="tagline-link">Join</a>
+                                <a href="#" class="tagline-link">Visit us on OrgSync</a>
+                              </div>
                       </div>
                     </div>
                   </div>
